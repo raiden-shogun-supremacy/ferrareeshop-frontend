6b65c10738df7750bc1f5385c007ca97521e4658
kaoapithepp <simsonslt.ig@gmail.com>
2021-09-29 19:43:45 +0700

    added filter version 1

--- src/Components/Pages/Product/ProductCard.js
+++ src/Components/Pages/Product/ProductCard.js
@@ -1,10 +1,27 @@
 import './ProductCard.css'
 
 const ProductCard = ({ detail }) => {
+
+    let cssVar = '';
+
+    switch(detail.Tag){
+        case 'In-stock':
+            cssVar = 'tag-instock';
+            break;
+        case 'Pre-order':
+            cssVar = 'tag-preorder';
+            break;
+    }
     return (
         <div className="card">
             <img src={detail.img} />
-            <span>{detail.ProductName}</span>
+            <p>{detail.ProductName}</p>
+            <p># {detail.ProdID}</p>
+            <span className={cssVar}>
+                {detail.Tag}
+            </span>
+            <span>{detail.Unitprice}</span>
+            <span>{detail.VendorName}, {detail.Height}</span>
         </div>
     );
 }
--- src/Components/Pages/Product/ProductCatalog.js
+++ src/Components/Pages/Product/ProductCatalog.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import dummy from '../../../Data/dummy.json';
 import ProductCard from './ProductCard';
 import '../PageStyle.css';
@@ -5,13 +6,22 @@ import '../PageStyle.css';
 
 const ProductCatalog = () => {
 
-    const render_product_showcase = dummy.map((data, key) => {
+    const [searchText, setSearchText] = useState('')
+
+    const render_product_showcase = dummy.filter((data) => {
+        return data.ProductName.toUpperCase().toLowerCase().includes(searchText);
+    }).map((data, key) => {
         return <ProductCard detail={data} index={key}/>
     });
     return (
         <div className="container">
-            <h1>Products 📦 </h1>
-            <h4>Let's make some order!</h4>
+            <div className="space-between">
+                <div className="header">
+                    <h1>Products 📦 </h1>
+                    <h4>Let's make some order!</h4>
+                </div>
+                <input className="input" placeholder="type to filter" value={searchText} onChange={(e) => setSearchText(e.target.value)}/>
+            </div>
             <div className="content">
                 <div className="wrapper">
                     {render_product_showcase}
