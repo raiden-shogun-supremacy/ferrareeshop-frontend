4b082b107e2b050fb7e0350c90bbff164e9d9bee
homly <khaohoms.1997@hotmail.com>
2021-10-09 23:28:55 +0700

    add login.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Dashboard from './Components/Dashboard';
 import Error404 from './Components/Pages/Error/Error404';
+import LoginStatus from './Components/SideBar/Logged/LoginBox';
+import Login from './Components/Pages/Login/Login';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={Dashboard} />
+        <Route exact path="/" component={Login}/>
+        <Route component={Dashboard} />
         <Route component={Error404} />
       </Switch>
     </BrowserRouter>
