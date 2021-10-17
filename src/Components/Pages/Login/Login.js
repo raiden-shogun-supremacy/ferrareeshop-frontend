b3e0cc1a762678ebee9f7ef3f3a540b5aca2026e
homly <khaohoms.1997@hotmail.com>
2021-10-17 17:34:45 +0700

    fix

--- src/Components/Pages/Login/Login.js
+++ src/Components/Pages/Login/Login.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import {Link, Redirect} from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './Login.css';
 
 const Login = () => {
@@ -11,34 +10,28 @@ const Login = () => {
     return (
 
         <>
-
-        <div class="jumbotron d-flex align-items-center min-vh-100">
-
-            <div className="container">
-                <img src="/logo/ferraree_logo.png"/>
-                <div className="text-center"><h1><strong>LOG IN</strong></h1></div>
+            <div className="login-container">
+                <center><img src="/logo/ferraree_logo.png"/></center>
+                <div><center><h1>LOG IN</h1></center></div>
                 <form style={{ width: "372px" }}>
-                <div className="mt-5 mb-3">
+                <div>
                     <label for="InputUsername" className="form-label">Username</label>
-                    <input type="text" name="username" className="form-control" 
+                    <br/><input type="text" name="username" className="form-control" 
                     id="InputUsername" placeholder="username"
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}/>
                 </div>
-                <div className="mt-4 mb-4">
-                    <label for="InputPassword" className="form-label">Password</label>
-                    <input type="password" name="password" className="form-control" 
+                <div>
+                    <br/><label for="InputPassword" className="form-label">Password</label>
+                    <br/><input type="password" name="password" className="form-control" 
                     id="InputPassword" placeholder="password" data-toggle="password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)} />
                 </div>
-                <div className="mt-3 mb-3">Don't have an account? <Link to ="/Register">Register here.</Link></div>
+                <div><br/>Don't have an account? <Link to ="/Register">Register here.</Link></div>
                 </form>
-                <div className="mt-5 text-center"><button type="submit" className="btn btn-primary shadow-none">LOG IN</button></div>
+                <center><br/><br/><button type="submit" className="btn btn-primary">LOG IN</button></center>
                 </div>
-
-        </div>
-
         </>
     );
 };
