543ff5edb3c4e7c9666776d977712abd955ae6a0
homly <khaohoms.1997@hotmail.com>
2021-10-24 16:04:52 +0700

    Add register page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import Dashboard from './Components/Dashboard';
 import Error404 from './Components/Pages/Error/Error404';
 import LoginStatus from './Components/SideBar/Logged/LoginBox';
 import Login from './Components/Pages/Login/Login';
+import Register from './Components/Pages/Register/Register';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={Login}/>
-        <Route component={Dashboard} />
-        <Route component={Error404} />
+        <Route exact path="/" component={Dashboard}/>
+        <Route path ="/login" component={Login} />
+        <Route path ="/register" component={Register} />
+        <Route path ="/dashboard" component={Dashboard} />
+        <Route path ="/error404" component={Error404} />
       </Switch>
     </BrowserRouter>
   );
