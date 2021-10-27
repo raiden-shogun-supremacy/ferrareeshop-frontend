e6e9301a8eb9907c4163eaa8858427f250e4dd8b
homly <khaohoms.1997@hotmail.com>
2021-10-28 03:36:44 +0700

    fix customer-register

--- src/Components/Pages/CustomerRegister/CustomerRegister.js
+++ src/Components/Pages/CustomerRegister/CustomerRegister.js
@@ -1,7 +1,6 @@
 import './CustomerRegister.css'
-import '../../../App.css'
 import {Link} from "react-router-dom";
-import {useStage} from 'react';
+import React, {useStage} from 'react';
 
 const CustomerRegister = () =>{
 
@@ -16,18 +15,18 @@ const CustomerRegister = () =>{
                 <div className="customer-register-content">
                     <h1>REGISTER</h1>
                 </div>
-                <form className="customer-register-form" >
+                <form action="" className="customer-register-form" >
                     <label for="fname">First Name
-                    <input type="text" id="fname" className="input" placeholder="first name" /></label>
+                    <input type="text" id="fname" className="input" placeholder="first name" required/></label>
                     <label for="lname">Last Name
-                    <input type="text" id="lname" className="input" placeholder="last name" /></label>
+                    <input type="text" id="lname" className="input" placeholder="last name" required/></label>
                     <label for="Address" className="customer-fullwidth">Address
-                    <input type="text" id="Address" className="input" placeholder="address" /></label>
+                    <input type="text" id="Address" className="input" placeholder="address" required/></label>
                     <label for="Phone">Phone
-                    <input type="int" id="Phone" className="input" placeholder="phone number" /></label>
+                    <input type="int" id="Phone" className="input" placeholder="phone number" required/></label>
                     <label for="PostalCode">Postcode
-                    <input type="int" id="PostalCode" className="input" placeholder="postcode" /></label>
-                    <button type="submit" className="btn-primary">REGISTER</button>
+                    <input type="int" id="PostalCode" className="input" placeholder="postcode" required/></label>
+                    <div align="center" className="customer-fullwidth"><button type="submit" className="btn-primary">REGISTER</button></div>
                 </form>
             </div>
         </div>
